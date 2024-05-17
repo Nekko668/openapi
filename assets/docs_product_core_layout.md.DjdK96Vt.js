50951866a499801153d6ffa5af7b389d5e57477b
Nekko <nekko668@gmail.com>
2024-05-17 10:05:05 +0000

    Deploy VitePress site

---
+++ assets/docs_product_core_layout.md.DjdK96Vt.js
@@ -0,0 +1,17 @@
+import{_ as a,c as s,o as n,a6 as p}from"./chunks/framework.CrBHAFc5.js";const u=JSON.parse('{"title":"布局说明","description":"","frontmatter":{},"headers":[],"relativePath":"docs/product/core/layout.md","filePath":"docs/product/core/layout.md","lastUpdated":1715939901000}'),e={name:"docs/product/core/layout.md"},l=p(`<h1 id="布局说明" tabindex="-1">布局说明 <a class="header-anchor" href="#布局说明" aria-label="Permalink to &quot;布局说明&quot;">​</a></h1><h2 id="目录结构" tabindex="-1">目录结构 <a class="header-anchor" href="#目录结构" aria-label="Permalink to &quot;目录结构&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>docs</span></span>
+<span class="line"><span>├── guide</span></span>
+<span class="line"><span>│   ├── core</span></span>
+<span class="line"><span>│   │   ├── 布局说明.md</span></span>
+<span class="line"><span>│   │   ├── 目录结构.md</span></span>
+<span class="line"><span>│   │   ├── 文档结构.md</span></span>
+<span class="line"><span>│   │   ├── 文档模板.md</span></span>
+<span class="line"><span>│   │   ├── 文档发布.md</span></span>
+<span class="line"><span>│   │   ├── 文档更新.md</span></span>
+<span class="line"><span>│   │   ├── 文档撰写规范.md</span></span>
+<span class="line"><span>│   │   ├── 文档贡献指南.md</span></span>
+<span class="line"><span>│   │   ├── 文档贡献者名单.md</span></span>
+<span class="line"><span>│   │   ├── 文档版本管理.md</span></span>
+<span class="line"><span>│   │   ├── 文档版本发布.md</span></span>
+<span class="line"><span>│   │   ├── 文档版本回退.md</span></span>
+<span class="line"><span>│   │   ├── 文档版本迁移.md</span></span>
+<span class="line"><span>│   │   ├── 文档版本归档.md</span></span></code></pre></div>`,3),t=[l];function c(o,d,i,r,m,_){return n(),s("div",null,t)}const f=a(e,[["render",c]]);export{u as __pageData,f as default};
